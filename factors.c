db19279296d8c0940fcd990321da94e85c228254
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-24 17:01:36 -0800

    Factors

---
+++ factors.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Compute the factors of numbers
+ * @ac: Number of args
+ * @av: Array of args
+ *
+ * Return: 0 if successful
+ * 1 if failed
+ */
+int main(int ac, char **av)
+{
+	FILE *fp;
+	char *lineread;
+	size_t lsize;
+	int num, i;
+
+	fp = fopen(av[1], "r");
+	if (fp == NULL)
+		return (1);
+	while (getline(&lineread, &lsize, fp) != -1)
+	{
+		num = atoi(lineread);
+		for (i = 0; i <= num; i++)
+		{
+			if (num % i == 0)
+			{
+				printf("%d=%d*%d", num, num, i);
+			}
+		}
+		free(lineread);
+	}
+	fclose(fp);
+	return (0);
+}
